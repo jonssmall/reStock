62733812e4a5954b85eb1f2e675ca2c7cf92ac1c
Jon Small <jonssmall@gmail.com>
2017-03-04 01:26:28 -0500

    dotenv configuration. quandl key hidden.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html');
-})
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
