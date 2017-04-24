21dac908c576fadb3dd27f57775ad8e6e6034e84
Jon S <jonssmall@gmail.com>
2017-04-24 00:45:19 -0400

    first working example of chartjs

--- client/app.js
+++ client/app.js
@@ -1,10 +1,12 @@
 const searchBtn = document.querySelector("#search-btn");
 searchBtn.onclick = () => {
   const symbol = document.querySelector("#symbol-input").value;  
-  ajaxGet(`/api/stocks?symbol=${symbol}`);
+  ajaxGet(`/api/stocks?symbol=${symbol}`, (res) => {
+    buildChart(res.dataset);
+  });
 };
 
-const ajaxGet = (url) => {
+const ajaxGet = (url, successCallback) => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${url}`);
   xhr.send(null);
@@ -14,7 +16,8 @@ const ajaxGet = (url) => {
     const OK = 200; 
     if (xhr.readyState === DONE) {
       if (xhr.status === OK) {
-        console.log(xhr.responseText);
+        //console.log(xhr.responseText);
+        successCallback(JSON.parse(xhr.responseText));
       } else {
         console.log('Error: ' + xhr.status);
       }
@@ -22,8 +25,23 @@ const ajaxGet = (url) => {
   };
 }
 
-window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer",
+const buildChart = (dataset) => {
+  let data = [];
+  let dataSeries = { type: "line" };
+  let dataPoints = [];
+
+  dataset.data.map(point => {
+    dataPoints.push({
+      x: new Date(point[0]),
+      y: point[4]
+    });
+  });  
+
+  dataSeries.dataPoints = dataPoints;
+  data.push(dataSeries);
+  console.log(data);
+
+	const chart = new CanvasJS.Chart("chartContainer",
 	{
 		zoomEnabled: false,
 		animationEnabled: false ,
@@ -39,48 +57,4 @@ window.onload = function () {
 		data: data  
 	});
 	chart.render();
-}
-
-var limit = 100000;    
-
-var y = 0;
-var data = []; 
-var dataSeries = { type: "line" };
-var dataPoints = [];
-for (var i = -limit/2; i <= limit/2; i++) {
-  y += (Math.random() * 10 - 5);
-  dateTime = new Date(1960, 08, 15);
-
-  // dateTime.setMilliseconds(dateTime.getMilliseconds() + i);
-  // dateTime.setSeconds(dateTime.getSeconds() + i);
-  // dateTime.setMinutes(dateTime.getMinutes() + i);
-  // dateTime.setHours(dateTime.getHours() + i);
-  dateTime.setDate(dateTime.getDate() + i);
-  // dateTime.setMonth(dateTime.getMonth() + i);
-  // dateTime.setFullYear(dateTime.getFullYear() + i);
-
-  dataPoints.push({
-    x: dateTime,
-    y: y
-  });
-}
-
-dataSeries.dataPoints = dataPoints;
-data.push(dataSeries);    
-
-// response.data =  [
-//       [
-//         "2017-04-20", date
-//         142.95,
-//         144.25,
-//         142.689,
-//         143.8, close
-//         15509334,
-//         0,
-//         1,
-//         142.95,
-//         144.25,
-//         142.689,
-//         143.8,
-//         15509334
-//       ], ...
+};
